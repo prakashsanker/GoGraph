7dd23000d2ac606961aeb5890e2784108d4b6187
psanker <psanker@splunk.com>
2015-10-10 13:43:22 -0700

    add number of edges check, take away magic numbers in test

--- graph_test.go
+++ graph_test.go
@@ -20,19 +20,15 @@ func TestAddEdge(t *testing.T) {
 	for _, node := range nodesToAdd {
 		g.AddNode(node.label)
 	}
-	i := 0
-	j := 0
-	for i < 6 {
-		for j < 6 {
+	for i , _ := range nodesToAdd {
+		for j, _ := range nodesToAdd {
 			g.AddEdge(i, j)
 			j++
 		} 
 		i++
 	}
-	i = 0
-	j = 0
-	for i < 6 {
-		for j < 6 {
+	for i, _ := range nodesToAdd {
+		for j, _ := range nodesToAdd {
 			hasEdge, err := g.HasEdge(i,j)
 			if err != nil {
 				fmt.Println(err)
@@ -40,42 +36,44 @@ func TestAddEdge(t *testing.T) {
 			if hasEdge == false {
 				t.Errorf("Graph does not have edge with start id %d and end id %d", i, j)
 			}
-			j++
 		}
-		i++
 	}
-
+	nEdges := g.NumberOfEdges()
+	expectedNEdges := len(nodesToAdd)*len(nodesToAdd)
+	if nEdges !=  expectedNEdges {
+		t.Errorf("Graph does not have edge for every node - number of edges is %d, when %d is expected", nEdges, expectedNEdges)
+	}
 }
 
-// func TestAddAndGetNode(t *testing.T) {
-// 	cases :=[]struct {
-// 			label string
-// 			expectedSize int
-// 		}{
-// 			{"1", 1},
-// 			{"2", 2},
-// 			{"3", 3},
-// 			{"4", 4},
-// 			{"5", 5},
-// 			{"6", 6},
-// 		}
-// 		g := New()
-// 		for _, c := range cases {
-// 			g.AddNode(c.label)
-// 			numberOfVertices := g.NumberOfVertices()
-// 			if numberOfVertices != c.expectedSize {
-// 				t.Errorf("Graph has %d vertices when %d were inserted", numberOfVertices, c.expectedSize)
-// 			}
-// 		}
+func TestAddAndGetNode(t *testing.T) {
+	cases :=[]struct {
+			label string
+			expectedSize int
+		}{
+			{"1", 1},
+			{"2", 2},
+			{"3", 3},
+			{"4", 4},
+			{"5", 5},
+			{"6", 6},
+		}
+		g := New()
+		for _, c := range cases {
+			g.AddNode(c.label)
+			numberOfVertices := g.NumberOfVertices()
+			if numberOfVertices != c.expectedSize {
+				t.Errorf("Graph has %d vertices when %d were inserted", numberOfVertices, c.expectedSize)
+			}
+		}
 
-// 		for i, c := range cases {
-// 			node, err := g.GetNode(i)
-// 			if err != nil {
-// 				fmt.Println(err)
-// 			}
-// 			if node.id != i && node.label != c.label {
-// 				t.Errorf("Graph has node with id %d and label %s at id %d", node.id, node.label, i)
-// 			}
-// 		}
-// }
+		for i, c := range cases {
+			node, err := g.GetNode(i)
+			if err != nil {
+				fmt.Println(err)
+			}
+			if node.id != i && node.label != c.label {
+				t.Errorf("Graph has node with id %d and label %s at id %d", node.id, node.label, i)
+			}
+		}
+}
 
