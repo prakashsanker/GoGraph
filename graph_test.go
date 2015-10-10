85c108d5a4df56ee944e43be8a7e59f15c5df9e5
psanker <psanker@splunk.com>
2015-10-10 13:14:21 -0700

    small test for getNode

--- graph_test.go
+++ graph_test.go
@@ -1,9 +1,11 @@
 package graph
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+	)
 
-func TestAddNode(t *testing.T) {
-
+func TestAddAndGetNode(t *testing.T) {
 	cases :=[]struct {
 			label string
 			expectedSize int
@@ -22,6 +24,17 @@ func TestAddNode(t *testing.T) {
 			numberOfVertices := g.NumberOfVertices()
 			if numberOfVertices != c.expectedSize {
 				t.Errorf("Graph has %d vertices when %d were inserted", numberOfVertices, c.expectedSize)
-			}		
+			}
+		}
+
+		for i, c := range cases {
+			node, err := g.GetNode(i)
+			if err != nil {
+				fmt.Println(err)
+			}
+			if node.id != i && node.label != c.label {
+				t.Errorf("Graph has node with id %d and label %s at id %d", node.id, node.label, i)
+			}
 		}
 }
+
