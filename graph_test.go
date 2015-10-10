5d164237664bb713c102a3c0f1ee44228ed9cd6f
psanker <psanker@splunk.com>
2015-10-10 13:33:43 -0700

    add test addEdge

--- graph_test.go
+++ graph_test.go
@@ -5,36 +5,77 @@ import (
 	"fmt"
 	)
 
-func TestAddAndGetNode(t *testing.T) {
-	cases :=[]struct {
+func TestAddEdge(t *testing.T) {
+	nodesToAdd := []struct {
 			label string
-			expectedSize int
-
 		}{
-			{"1", 1},
-			{"2", 2},
-			{"3", 3},
-			{"4", 4},
-			{"5", 5},
-			{"6", 6},
+			{"1"},
+			{"2"},
+			{"3"},
+			{"4"},
+			{"5"},
+			{"6"},
 		}
-		g := New()
-		for _, c := range cases {
-			g.AddNode(c.label)
-			numberOfVertices := g.NumberOfVertices()
-			if numberOfVertices != c.expectedSize {
-				t.Errorf("Graph has %d vertices when %d were inserted", numberOfVertices, c.expectedSize)
-			}
-		}
-
-		for i, c := range cases {
-			node, err := g.GetNode(i)
+	g := New()
+	for _, node := range nodesToAdd {
+		g.AddNode(node.label)
+	}
+	i := 0
+	j := 0
+	for i < 6 {
+		for j < 6 {
+			g.AddEdge(i, j)
+			j++
+		} 
+		i++
+	}
+	i = 0
+	j = 0
+	for i < 6 {
+		for j < 6 {
+			hasEdge, err := g.HasEdge(i,j)
 			if err != nil {
 				fmt.Println(err)
 			}
-			if node.id != i && node.label != c.label {
-				t.Errorf("Graph has node with id %d and label %s at id %d", node.id, node.label, i)
+			if hasEdge == false {
+				t.Errorf("Graph does not have edge with start id %d and end id %d", i, j)
 			}
+			j++
 		}
+		i++
+	}
+
 }
+
+// func TestAddAndGetNode(t *testing.T) {
+// 	cases :=[]struct {
+// 			label string
+// 			expectedSize int
+// 		}{
+// 			{"1", 1},
+// 			{"2", 2},
+// 			{"3", 3},
+// 			{"4", 4},
+// 			{"5", 5},
+// 			{"6", 6},
+// 		}
+// 		g := New()
+// 		for _, c := range cases {
+// 			g.AddNode(c.label)
+// 			numberOfVertices := g.NumberOfVertices()
+// 			if numberOfVertices != c.expectedSize {
+// 				t.Errorf("Graph has %d vertices when %d were inserted", numberOfVertices, c.expectedSize)
+// 			}
+// 		}
+
+// 		for i, c := range cases {
+// 			node, err := g.GetNode(i)
+// 			if err != nil {
+// 				fmt.Println(err)
+// 			}
+// 			if node.id != i && node.label != c.label {
+// 				t.Errorf("Graph has node with id %d and label %s at id %d", node.id, node.label, i)
+// 			}
+// 		}
+// }
 
